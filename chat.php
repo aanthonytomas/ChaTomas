06312e0e9136cddd87273c5d0c00c1eac2353b73
Mark Anthony Tomas <aanthonytomas2@gmail.com>
2025-05-09 00:02:39 +0800

    Update chat.php

--- chat.php
+++ chat.php
@@ -6,7 +6,7 @@ redirect_if_not_logged_in();
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en"> 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
